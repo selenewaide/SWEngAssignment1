31272f51412550c8d7a7195b2c6ec243ca2bfd11
Selene Waide <selenewaide@Selenes-MacBook-Pro.local>
2017-01-25 18:01:00 +0000

    Initial commit

---
+++ assignment1.py
@@ -0,0 +1,29 @@
+'''
+Created on 25 Jan 2017
+
+@author: selenewaide
+'''
+
+import platform
+
+print("Machine: ", platform.machine())
+
+print("Version: ", platform.version())
+
+print("Platform: ", platform.platform())
+
+print("Uname: ", platform.uname())
+
+print("System: ", platform.system())
+
+print("Processor: ", platform.processor())
+
+
+ 
+
+ 
+
+ 
+
+ 
+
---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='assignment1',
+      version='1.0',
+      py_modules=['assignment1'],
+      )
